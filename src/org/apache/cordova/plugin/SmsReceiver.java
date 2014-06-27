053ff4feaa80e6bf2dda9b7b8c1fb39cf3f7b29e
neerajtuteja <neeraj.tuteja+github@gmail.com>
2014-06-28 00:19:23 +0530

    Update SmsReceiver.java

--- src/org/apache/cordova/plugin/SmsReceiver.java
+++ src/org/apache/cordova/plugin/SmsReceiver.java
@@ -1,5 +1,7 @@
 /*
 Copyright (C) 2013 by Pierre-Yves Orban
+Copyright (C) 2014 by Neeraj Tuteja
+%
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
